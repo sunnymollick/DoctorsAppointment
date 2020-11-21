b91ae2a0bf7da902bc19b45d5ed8f2b4b35c397c
Avishek Barua <avishekbarua02@gmail.com>
2020-11-22 00:35:39 +0600

    deleted some files

--- resources/views/admin/includes/single/head.blade.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<meta name="description" content="" />
-<meta name="author" content="" />
-<title>Page Title - SB Admin</title>
-<link href="{{ asset('ui/admin') }}/css/styles.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
